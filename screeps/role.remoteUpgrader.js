715d8a44369709841857f93ad3f81b01ffb352e2
Mark Steven Alan Thomas <mark@thomas.as>
2018-01-09 22:50:23 +0100

    add correct withdraw for upgrader

--- screeps/role.remoteUpgrader.js
+++ screeps/role.remoteUpgrader.js
@@ -21,7 +21,7 @@ var roleRemoteUpgrader = {
                 }
             });
             if (source) {
-                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                if (creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(source);
                 }
             }

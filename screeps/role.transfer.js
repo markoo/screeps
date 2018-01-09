41f8d1b384996bd66907d37d3e6ac00f5bbfd0b3
Mark Steven Alan Thomas <mark@thomas.as>
2018-01-09 22:34:11 +0100

    add simplified harvest to transfer when idle

--- screeps/role.transfer.js
+++ screeps/role.transfer.js
@@ -1,11 +1,11 @@
 var roleTransfer = {
-    
+
     /** @param {Creep} creep **/
-    run: function(creep) {
-        if(creep.memory.transfering && creep.carry.energy == 0) {
+    run: function (creep) {
+        if (creep.memory.transfering && creep.carry.energy == 0) {
             creep.memory.transfering = false;
         }
-        if(!creep.memory.transfering && creep.carry.energy == creep.carryCapacity) {
+        if (!creep.memory.transfering && creep.carry.energy == creep.carryCapacity) {
             creep.memory.transfering = true;
         }
         var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
@@ -15,35 +15,43 @@ var roleTransfer = {
             }
         });
 
-        if(creep.memory.transfering) {
-            if(target) {
-                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+        if (creep.memory.transfering) {
+            if (target) {
+                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
                 }
-            }else{
+            } else {
                 var theStorage = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_STORAGE);
                     }
                 });
-    
+
                 if (creep.transfer(theStorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(theStorage);
                 }
-   
+
             }
         }
         else {
-            var theStorage = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_STORAGE );
+            if (target) {
+                var theStorage = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_STORAGE);
+                    }
+                });
+
+                if (creep.withdraw(theStorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(theStorage);
                 }
-            });
-            
-            if ( creep.withdraw(theStorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo( theStorage );
+            } else {
+                const source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+                if (source) {
+                    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(source);
+                    }
+                }
             }
-          
         }
     }
 };

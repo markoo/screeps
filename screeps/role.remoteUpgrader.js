7dc97a5409eb0de9491df7e1fe9bf5691e9f7e4f
Mark Steven Alan Thomas <mark@thomas.as>
2018-01-09 22:44:07 +0100

    add containers and storage to upgrader

--- screeps/role.remoteUpgrader.js
+++ screeps/role.remoteUpgrader.js
@@ -1,43 +1,31 @@
 var roleRemoteUpgrader = {
-    
-    /** @param {Creep} creep **/
-    run: function(creep) {
-if(creep.memory.upgrading && creep.carry.energy == 0) {
-        creep.memory.upgrading = false;
-    }
-    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-        creep.memory.upgrading = true;
-    }
 
-    if(creep.memory.upgrading) {
-        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(creep.room.controller);
+    /** @param {Creep} creep **/
+    run: function (creep) {
+        if (creep.memory.upgrading && creep.carry.energy == 0) {
+            creep.memory.upgrading = false;
         }
-    }
-    else {
-        var theStorage = creep.room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_CONTAINER);
-            }
-        });
-        if(theStorage[2].store[RESOURCE_ENERGY] > 0 || theStorage[4].store[RESOURCE_ENERGY] > 0){
-            if(theStorage[2].store[RESOURCE_ENERGY] > 0){
-                if (creep.withdraw( theStorage[2], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo( theStorage[2] );
-                }
-            }
-            if(theStorage[4].store[RESOURCE_ENERGY] > 0){
-                if (creep.withdraw( theStorage[4], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo( theStorage[4] );
-                }
-            }
-        }else{
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[1]);
+        if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.upgrading = true;
+        }
+
+        if (creep.memory.upgrading) {
+            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
             }
         }
-    }
+        else {
+            const source = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => {;
+                    return (structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_STORAGE);
+                }
+            });
+            if (source) {
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+            }
+        }
     }
 };
 

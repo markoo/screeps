d87f973a7d564a43f876db948ba2b912858a14c0
Mark Steven Alan Thomas <mark@thomas.as>
2018-01-14 16:30:22 +0100

    fix upgrader code

--- screeps/role.upgrader.js
+++ screeps/role.upgrader.js
@@ -16,13 +16,13 @@ if(creep.memory.upgrading && creep.carry.energy == 0) {
     }
     else {
             var sources = creep.room.find(FIND_SOURCES);
-        if(creep.room.name != 'W43N3'){
+        if(creep.room.name == 'W43N3'){
             if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1]);
             }
         }else{
         }
-        if(!sources[1].energy || creep.room.name != 'W43N2'){
+        if(!sources[1].energy && creep.room.name != 'W43N2'){
             var theStorage = creep.pos.findClosestByRange(FIND_MY_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType == STRUCTURE_LINK || structure.structureType == STRUCTURE_STORAGE);
